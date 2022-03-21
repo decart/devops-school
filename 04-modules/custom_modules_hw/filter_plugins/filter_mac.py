32ac6ce98cde839b7202376fac4380502543e4a9
Mansur <yaponomat@gmail.com>
2022-03-21 19:27:36 +0300

    Add custom filter

---
+++ 04-modules/custom_modules_hw/filter_plugins/filter_mac.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import re
+from ansible.errors import (
+    AnsibleFilterTypeError
+)
+
+
+def mac(val):
+    if not isinstance(val, str):
+        raise AnsibleFilterTypeError("TypeError. Input value must be a sting")
+
+    if re.search('[\da-f]{12}', val, re.IGNORECASE) == None:
+        raise AnsibleFilterTypeError(
+            "TypeError. Input value has a wrong format")
+
+    return re.sub("([\da-f]{2})\B", "\\1:", val, 0, re.IGNORECASE)
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'mac': mac
+        }
